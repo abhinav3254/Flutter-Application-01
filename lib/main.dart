8f4cd11cd0dd35f451c23c77d32040bc1f1b83e1
abhinav3254 <abhinav3254@gmail.com>
2022-12-09 15:54:56 +0530

    trying things by own

--- lib/main.dart
+++ lib/main.dart
@@ -14,3 +14,17 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Text('Hello Abhinav'),
+//     );
+//   }
+// }
