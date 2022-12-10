7d0066125e14b9563acb060df24398200184558e
abhinav3254 <abhinav3254@gmail.com>
2022-12-10 22:26:26 +0530

    multiple widgets

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var questions = ['What\'s your fav color?', 'What\'s your fav animal?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First APP'),
         ),
-        body: const Text('This is my default Text'),
+        body: Column(
+          children: const [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 4'),
+            ),
+          ],
+        ),
       ),
     );
   }
