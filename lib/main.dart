a8bca4e9a4290ab2bc516ca28a4e277c2a76b6de
abhinav3254 <abhinav3254@gmail.com>
2022-12-11 21:56:06 +0530

    removing some sort of useless code

--- lib/main.dart
+++ lib/main.dart
@@ -44,17 +44,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Text('Hello Abhinav'),
-//     );
-//   }
-// }
